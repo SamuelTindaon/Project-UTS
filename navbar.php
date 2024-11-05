f9f4822d35c5808bbd6bc144f8ab72d45bf98be1
SAMUEL DAMATTA TINDAON <147571483+SamuelTindaon@users.noreply.github.com>
2024-11-05 22:41:05 +0700

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,22 @@
+<!-- navbar.php -->
+<nav class="navbar navbar-expand-lg navbar-dark">
+    <div class="container">
+        <a class="navbar-brand" href="#">Transformers Universe</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#home">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#characters">Characters</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#contact">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
